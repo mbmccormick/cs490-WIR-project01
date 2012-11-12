04831a454136c58110c2180da5bc2fcec4705948
Matt McCormick <mbmccormick@outlook.com>
2012-11-11 23:25:39 -0500

    Adding better Execute method.

--- Execute.aspx.cs
+++ Execute.aspx.cs
@@ -8,6 +8,7 @@ using System.Web.UI.WebControls;
 using System.Web.Helpers;
 using System.Data;
 using System.IO;
+using System.Text;
 
 namespace BillboardAnalyzer
 {
@@ -21,13 +22,22 @@ namespace BillboardAnalyzer
             WebClient client = new WebClient();
 
             // download billboard top 100 songs
+            Response.Write("Downloading Billboard Top 100 chart.<br />\n");
             string response1 = client.DownloadString("http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=100&country=us&f_has_lyrics=1&apikey=" + apiKey);
             
             // parse songs from chart response
             var chart = Json.Decode(response1).message.body.track_list;
+
+            StringBuilder sb = new StringBuilder();
+
+            // loop through songs in chart
+            int i = 1;
             foreach (var song in chart)
             {
+                sb.AppendLine(song.track.track_id + ";" + song.track.track_name + ";" + song.track.artist_name);
+                
                 // download lyrics for this song
+                Response.Write("Downloading lyrics for #" + i + " " + song.track.track_name + " by " + song.track.artist_name + ".<br />\n");
                 string response2 = client.DownloadString("http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=" + song.track.track_id + "&apikey=" + apiKey);
 
                 // parse lyrics for track response
@@ -35,10 +45,17 @@ namespace BillboardAnalyzer
 
                 // save lyrics to disk
                 File.WriteAllText(Server.MapPath("~/Data") + "\\" + song.track.track_id + ".txt", lyrics);
+
+                i++;
             }
+
+            // save index to disk
+            File.WriteAllText(Server.MapPath("~/Data") + "\\index.txt", sb.ToString());
 
             // dispose web client
             client.Dispose();
+
+            Response.Write("Done.<br />\n");
         }
     }
 }
